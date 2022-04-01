3f37631da5b2fb6167e3b0f62e684be9c6d25336
gumingWu <15626185054@163.com>
2022-04-01 12:51:11 +0800

    初尝解析功能

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
-<template>im app</template>
+<template>
+  text: {{ text }}
+  <br />
+  compiler: {{ compiler }}
+  <br />
+  v3: {{ v3 }}
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { compile } from "vue-template-compiler";
+import { compile as compileV3 } from "@vue/compiler-dom";
+
+const text = `
+  <div>hi</div>
+`;
+
+const compiler = compile(text);
+
+const v3 = compileV3(text);
+</script>
